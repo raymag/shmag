0353cdc14fccc78826b1f8f57621785ba054d4d9
raymag <prayk11@outlook.com>
2020-08-05 10:48:45 -0300

    feat: version one

--- server.ts
+++ server.ts
@@ -1,17 +1,26 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const yup = require('yup');
-const nanoid = require('nanoid');
-const cors = require('cors');
+const monk = require('monk');
+const { nanoid } = require('nanoid');
+
+require('dotenv').config();
+
+const db = monk(process.env.MONGODB_URI);
+const urls = db.get('urls');
+urls.createIndex({ slug: 1 }, { unique: true });
 
 const app = express();
+app.enable('trust proxy');
 
 app.use(helmet());
 app.use(morgan('common'));
 app.use(express.json())
 app.use(express.static('./public'))
-app.use(cors());
+
+const notFoundPath = path.join(__dirname, 'public/404.html');
 
 const schema = yup.object().shape({
     slug: yup.string().trim().matches(/^[\w\-]+$/i),
@@ -19,28 +28,62 @@ const schema = yup.object().shape({
 });
 
 app.get('/', (req, res) => {
-    res.send('Code is Pika')
+    res.send('SHMAG shortning links for you!');
 });
 
-// app.post('/url', async (req, res) => {
-//     let { slug, url } = req.body;
-//     try {
-//         await schema.validate({slug, url});
-//         if(url.includes('rmag.tk')){
-//             throw new Error('Don\'t.');
-//         }
-//         if(!slug){
-//             slug = nanoid(5);
-//         }else{
-//             const existing = await.
-//         }
+app.get('/:id', async (req, res, next) => {
+    const { id: slug } = req.params;
+    try {
+        const url = await urls.findOne({ slug });
+        if (url){
+            return res.redirect(url.url);
+        }else {
+            return res.status(404).sendFile(notFoundPath);
+        }
+    } catch (error) {
+        return res.status(404).sendFile(notFoundPath);
+    }
+});
 
-//     } catch (error) {
-        
-//     }
-// });
+app.post('/url', async (req, res, next) => {
+    let { slug, url } = req.body;
+    try {
+        await schema.validate({slug, url});
+        if ( url.includes('rmag.tk') ) {
+            throw new Error('Don\'t.');
+        }
+        if (!slug) {
+            slug = nanoid(5);
+        } else {
+            const existing = await urls.findOne({ slug });
+            if ( existing ) {
+                throw new Error('Slug already in use.');
+            }
+        }
+        slug = slug.toLowerCase();
+        const newUrl = { url, slug };
+        const created = await urls.insert( newUrl );
+        res.json( created );
+    } catch (error) {
+        next(error);
+    }
+});
 
+app.use((req, res, next) => {
+    res.status(404).sendFile(notFoundPath);
+});
 
+app.use((error, req, res, next) => {
+    if (error.status) {
+        res.status(error.status);
+    } else {
+        res.status(500);
+    }
+    res.json({
+        message: error.message,
+        stack: process.env.NODE_ENV === 'production' ? ':o' : error.stack
+    });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
